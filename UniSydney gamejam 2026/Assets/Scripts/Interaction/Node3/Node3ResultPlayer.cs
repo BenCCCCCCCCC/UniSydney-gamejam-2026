905dd374786928b79b5edfa9737ed5079bf35823
SoapNight <soapnighte@gmail.com>
2026-06-25 23:17:58 +1000

    asw

--- UniSydney gamejam 2026/Assets/Scripts/Interaction/Node3/Node3ResultPlayer.cs
+++ UniSydney gamejam 2026/Assets/Scripts/Interaction/Node3/Node3ResultPlayer.cs
@@ -1,6 +1,10 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 #if UNITY_EDITOR
 using UnityEditor.SceneManagement;
@@ -14,7 +18,6 @@ public class Node3ResultPlayer : MonoBehaviour
     [SerializeField] private StoryActorAutoMove storyActor;
 
     [Header("Dialogue")]
-    [SerializeField] private SceneTextUIController textUI;
     [SerializeField] private float messageDuration = 1.6f;
 
     private bool routeSolved;
@@ -24,6 +27,13 @@ public class Node3ResultPlayer : MonoBehaviour
 
     private string routeFailReason = "Snow White is lost.";
     private string doorFailReason = "Snow White couldn't enter the house.";
+
+    private GameObject canvasObject;
+    private GameObject dialoguePanelObject;
+    private TMP_Text dialogueText;
+
+    private GameObject endingPanelObject;
+    private TMP_Text endingText;
 
     private Coroutine dialogueCoroutine;
 
@@ -44,16 +54,9 @@ public class Node3ResultPlayer : MonoBehaviour
             storyActor = FindAnyObjectByType<StoryActorAutoMove>();
         }
 
-        if (textUI != null)
-        {
-            textUI.ConfigureEndingButtons(RetryNode3, RetryNode3, HandleNextLevel);
-            textUI.HideDialogue();
-            textUI.HideEnding();
-        }
-        else
-        {
-            Debug.LogWarning("Node3ResultPlayer: SceneTextUIController is not assigned.");
-        }
+        BuildRuntimeUI();
+        HideDialogue();
+        HideEndingPanel();
     }
 
     public void PlayResult(PlacementPoint point)
@@ -199,35 +202,133 @@ public class Node3ResultPlayer : MonoBehaviour
         ShowEndingPanel(true, "Snow White settles down in the forest.");
     }
 
-    private void ShowDialogue(string message)
+    private void BuildRuntimeUI()
     {
-        if (textUI == null)
+        EnsureEventSystem();
+
+        if (canvasObject != null)
         {
-            Debug.LogWarning("Node3ResultPlayer: cannot show dialogue because SceneTextUIController is not assigned.");
             return;
         }
 
-        textUI.ShowDialogue(message);
+        canvasObject = new GameObject(
+            "Node3ResultCanvas",
+            typeof(RectTransform),
+            typeof(Canvas),
+            typeof(CanvasScaler),
+            typeof(GraphicRaycaster));
+
+        Canvas canvas = canvasObject.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        CanvasScaler scaler = canvasObject.GetComponent<CanvasScaler>();
+        scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        scaler.referenceResolution = new Vector2(1920f, 1080f);
+        scaler.matchWidthOrHeight = 0.5f;
+
+        RectTransform canvasRect = canvasObject.GetComponent<RectTransform>();
+
+        dialoguePanelObject = CreatePanel(
+            "DialoguePanel",
+            canvasRect,
+            new Vector2(0.5f, 0.16f),
+            new Vector2(980f, 130f),
+            new Color(0.05f, 0.055f, 0.07f, 0.92f));
+
+        dialogueText = CreateText(
+            "DialogueText",
+            dialoguePanelObject.transform,
+            "",
+            new Vector2(0.5f, 0.5f),
+            new Vector2(900f, 90f),
+            34f);
+    }
+
+    private void ShowDialogue(string message)
+    {
+        BuildRuntimeUI();
+
+        dialoguePanelObject.SetActive(true);
+        dialogueText.text = message;
     }
 
     private void HideDialogue()
     {
-        if (textUI != null)
+        if (dialoguePanelObject != null)
         {
-            textUI.HideDialogue();
+            dialoguePanelObject.SetActive(false);
         }
     }
 
     private void ShowEndingPanel(bool success, string message)
     {
-        if (textUI == null)
-        {
-            Debug.LogWarning("Node3ResultPlayer: cannot show ending because SceneTextUIController is not assigned.");
-            return;
-        }
+        BuildRuntimeUI();
+
+        HideEndingPanel();
+
+        RectTransform canvasRect = canvasObject.GetComponent<RectTransform>();
+
+        endingPanelObject = CreatePanel(
+            "EndingPanel",
+            canvasRect,
+            new Vector2(0.5f, 0.5f),
+            new Vector2(900f, 420f),
+            new Color(0.04f, 0.045f, 0.06f, 0.96f));
 
         string title = success ? "Success" : "Failed";
-        textUI.ShowEnding(title, message, success);
+        string body = $"{title}\n\n{message}";
+
+        endingText = CreateText(
+            "EndingText",
+            endingPanelObject.transform,
+            body,
+            new Vector2(0.5f, 0.66f),
+            new Vector2(780f, 190f),
+            38f);
+
+        if (success)
+        {
+            Button tryAnotherWayButton = CreateButton(
+                "TryAnotherWayButton",
+                endingPanelObject.transform,
+                "Try Another Way",
+                new Vector2(0.37f, 0.22f),
+                new Vector2(260f, 76f));
+
+            tryAnotherWayButton.onClick.AddListener(RetryNode3);
+
+            Button nextButton = CreateButton(
+                "NextLevelButton",
+                endingPanelObject.transform,
+                "Next Level",
+                new Vector2(0.67f, 0.22f),
+                new Vector2(220f, 76f));
+
+            nextButton.onClick.AddListener(() =>
+            {
+                Debug.Log("Next Level button clicked. Not implemented yet.");
+            });
+        }
+        else
+        {
+            Button retryButton = CreateButton(
+                "RetryButton",
+                endingPanelObject.transform,
+                "Retry",
+                new Vector2(0.5f, 0.22f),
+                new Vector2(220f, 76f));
+
+            retryButton.onClick.AddListener(RetryNode3);
+        }
+    }
+
+    private void HideEndingPanel()
+    {
+        if (endingPanelObject != null)
+        {
+            Destroy(endingPanelObject);
+            endingPanelObject = null;
+        }
     }
 
     private void RetryNode3()
@@ -238,11 +339,6 @@ public class Node3ResultPlayer : MonoBehaviour
         GameSessionData.ToolCardIDs.Clear();
 
         LoadSceneByName(retrySceneName);
-    }
-
-    private void HandleNextLevel()
-    {
-        Debug.Log("Next Level button clicked. Not implemented yet.");
     }
 
     private bool IsRunningInRetryScene()
@@ -300,5 +396,93 @@ public class Node3ResultPlayer : MonoBehaviour
 #endif
 
         SceneManager.LoadScene(sceneName);
+    }
+
+    private GameObject CreatePanel(string name, Transform parent, Vector2 anchor, Vector2 size, Color color)
+    {
+        GameObject panelObject = new GameObject(name, typeof(RectTransform), typeof(Image));
+        panelObject.transform.SetParent(parent, false);
+
+        Image image = panelObject.GetComponent<Image>();
+        image.color = color;
+
+        RectTransform rect = panelObject.GetComponent<RectTransform>();
+        rect.anchorMin = anchor;
+        rect.anchorMax = anchor;
+        rect.pivot = new Vector2(0.5f, 0.5f);
+        rect.sizeDelta = size;
+        rect.anchoredPosition = Vector2.zero;
+
+        return panelObject;
+    }
+
+    private TMP_Text CreateText(string name, Transform parent, string text, Vector2 anchor, Vector2 size, float fontSize)
+    {
+        GameObject textObject = new GameObject(name, typeof(RectTransform), typeof(TextMeshProUGUI));
+        textObject.transform.SetParent(parent, false);
+
+        TMP_Text tmp = textObject.GetComponent<TMP_Text>();
+        tmp.text = text;
+        tmp.alignment = TextAlignmentOptions.Center;
+        tmp.color = Color.white;
+        tmp.fontSize = fontSize;
+        tmp.textWrappingMode = TextWrappingModes.Normal;
+        tmp.enableAutoSizing = true;
+        tmp.fontSizeMin = 18f;
+        tmp.fontSizeMax = fontSize;
+
+        RectTransform rect = textObject.GetComponent<RectTransform>();
+        rect.anchorMin = anchor;
+        rect.anchorMax = anchor;
+        rect.pivot = new Vector2(0.5f, 0.5f);
+        rect.sizeDelta = size;
+        rect.anchoredPosition = Vector2.zero;
+
+        return tmp;
+    }
+
+    private Button CreateButton(string name, Transform parent, string label, Vector2 anchor, Vector2 size)
+    {
+        GameObject buttonObject = new GameObject(name, typeof(RectTransform), typeof(Image), typeof(Button));
+        buttonObject.transform.SetParent(parent, false);
+
+        Image image = buttonObject.GetComponent<Image>();
+        image.color = new Color(0.18f, 0.35f, 0.52f, 0.98f);
+
+        RectTransform rect = buttonObject.GetComponent<RectTransform>();
+        rect.anchorMin = anchor;
+        rect.anchorMax = anchor;
+        rect.pivot = new Vector2(0.5f, 0.5f);
+        rect.sizeDelta = size;
+        rect.anchoredPosition = Vector2.zero;
+
+        Button button = buttonObject.GetComponent<Button>();
+
+        TMP_Text text = CreateText(
+            "Label",
+            buttonObject.transform,
+            label,
+            new Vector2(0.5f, 0.5f),
+            size,
+            28f);
+
+        text.fontStyle = FontStyles.Bold;
+
+        return button;
+    }
+
+    private void EnsureEventSystem()
+    {
+        if (FindAnyObjectByType<EventSystem>() != null)
+        {
+            return;
+        }
+
+        GameObject eventSystemObject = new GameObject(
+            "EventSystem",
+            typeof(EventSystem),
+            typeof(InputSystemUIInputModule));
+
+        eventSystemObject.SetActive(true);
     }
 }
--- UniSydney gamejam 2026/Assets/Scripts/Interaction/Node5/Node5SimpleFlowController.cs
+++ UniSydney gamejam 2026/Assets/Scripts/Interaction/Node5/Node5SimpleFlowController.cs
@@ -1,6 +1,10 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 #if UNITY_EDITOR
 using UnityEditor.SceneManagement;
@@ -14,8 +18,12 @@ public class Node5SimpleFlowController : MonoBehaviour
     [SerializeField] private string cardBackpackSceneName = "CardBackpackTest";
 
     [Header("Briefing")]
-    [SerializeField] private SceneTextUIController textUI;
+    [SerializeField] private string briefingText = "Father: \"Finally, the prince arrived at the crystal coffin...\"\nChild: \"Wait. This time, I decide the ending.\"";
     [SerializeField] private float briefingDuration = 3.2f;
+
+    private GameObject canvasObject;
+    private GameObject briefingPanelObject;
+    private TMP_Text briefingTMP;
 
     private IEnumerator Start()
     {
@@ -30,28 +38,103 @@ public class Node5SimpleFlowController : MonoBehaviour
         GameSessionData.CardBackpackSceneName = cardBackpackSceneName;
         GameSessionData.CurrentPhase = GameFlowPhase.Briefing;
 
-        if (textUI != null)
-        {
-            textUI.HideAll();
-            textUI.ShowBriefing();
-        }
-        else
-        {
-            Debug.LogWarning("Node5SimpleFlowController: SceneTextUIController is not assigned.");
-        }
+        BuildBriefingUI();
+        ShowBriefing(briefingText);
 
         yield return new WaitForSeconds(briefingDuration);
 
-        if (textUI != null)
-        {
-            textUI.HideBriefing();
-        }
+        HideBriefing();
 
         GameSessionData.CurrentPhase = GameFlowPhase.CardCrafting;
 
         Debug.Log($"Node5SimpleFlowController: loading scene {cardBackpackSceneName}");
 
         LoadSceneByName(cardBackpackSceneName);
+    }
+
+    private void BuildBriefingUI()
+    {
+        EnsureEventSystem();
+
+        if (canvasObject != null)
+        {
+            return;
+        }
+
+        canvasObject = new GameObject(
+            "Node5BriefingCanvas",
+            typeof(RectTransform),
+            typeof(Canvas),
+            typeof(CanvasScaler),
+            typeof(GraphicRaycaster));
+
+        Canvas canvas = canvasObject.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        CanvasScaler scaler = canvasObject.GetComponent<CanvasScaler>();
+        scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        scaler.referenceResolution = new Vector2(1920f, 1080f);
+        scaler.matchWidthOrHeight = 0.5f;
+
+        RectTransform canvasRect = canvasObject.GetComponent<RectTransform>();
+
+        briefingPanelObject = new GameObject(
+            "BriefingPanel",
+            typeof(RectTransform),
+            typeof(Image));
+
+        briefingPanelObject.transform.SetParent(canvasRect, false);
+
+        Image image = briefingPanelObject.GetComponent<Image>();
+        image.color = new Color(0.04f, 0.045f, 0.06f, 0.92f);
+
+        RectTransform panelRect = briefingPanelObject.GetComponent<RectTransform>();
+        panelRect.anchorMin = new Vector2(0.5f, 0.18f);
+        panelRect.anchorMax = new Vector2(0.5f, 0.18f);
+        panelRect.pivot = new Vector2(0.5f, 0.5f);
+        panelRect.sizeDelta = new Vector2(1120f, 190f);
+        panelRect.anchoredPosition = Vector2.zero;
+
+        GameObject textObject = new GameObject(
+            "BriefingText",
+            typeof(RectTransform),
+            typeof(TextMeshProUGUI));
+
+        textObject.transform.SetParent(briefingPanelObject.transform, false);
+
+        briefingTMP = textObject.GetComponent<TMP_Text>();
+        briefingTMP.alignment = TextAlignmentOptions.Center;
+        briefingTMP.color = Color.white;
+        briefingTMP.fontSize = 34f;
+        briefingTMP.textWrappingMode = TextWrappingModes.Normal;
+        briefingTMP.enableAutoSizing = true;
+        briefingTMP.fontSizeMin = 18f;
+        briefingTMP.fontSizeMax = 34f;
+
+        RectTransform textRect = textObject.GetComponent<RectTransform>();
+        textRect.anchorMin = Vector2.zero;
+        textRect.anchorMax = Vector2.one;
+        textRect.offsetMin = new Vector2(40f, 20f);
+        textRect.offsetMax = new Vector2(-40f, -20f);
+    }
+
+    private void ShowBriefing(string message)
+    {
+        if (briefingPanelObject == null || briefingTMP == null)
+        {
+            return;
+        }
+
+        briefingPanelObject.SetActive(true);
+        briefingTMP.text = message;
+    }
+
+    private void HideBriefing()
+    {
+        if (briefingPanelObject != null)
+        {
+            briefingPanelObject.SetActive(false);
+        }
     }
 
     private void LoadSceneByName(string sceneName)
@@ -69,5 +152,20 @@ public class Node5SimpleFlowController : MonoBehaviour
 #endif
 
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void EnsureEventSystem()
+    {
+        if (FindAnyObjectByType<EventSystem>() != null)
+        {
+            return;
+        }
+
+        GameObject eventSystemObject = new GameObject(
+            "EventSystem",
+            typeof(EventSystem),
+            typeof(InputSystemUIInputModule));
+
+        eventSystemObject.SetActive(true);
     }
 }
